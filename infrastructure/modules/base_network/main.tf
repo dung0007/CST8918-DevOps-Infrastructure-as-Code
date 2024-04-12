479c77caee020fc938d8a09119ebff1a9a9d585a
TheTeeThailand <144491675+TheTeeThailand@users.noreply.github.com>
2024-04-11 20:01:33 -0400

    network

---
+++ infrastructure/modules/base_network/main.tf
@@ -0,0 +1,55 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "network" {
+  name     = "cst8918-final-project-group-fp3"
+  location = "West US 3"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  address_space       = ["10.0.0.0/14"]
+  location            = azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+}
+
+resource "azurerm_subnet" "prod" {
+  name                 = "prod"
+  resource_group_name  = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "test"
+  resource_group_name  = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet" "dev" {
+  name                 = "dev"
+  resource_group_name  = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.2.0.0/16"]
+}
+
+resource "azurerm_subnet" "admin" {
+  name                 = "admin"
+  resource_group_name  = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.3.0.0/16"]
+}
